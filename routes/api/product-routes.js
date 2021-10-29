73e25ed5e59f359dfe589242cfc61d418a7f27b9
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-28 20:17:07 -0700

    grab all produces

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,17 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const result = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag, as: 'product_info' },
+      ],
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(418).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+router.get('/:id', async (req, res) => {
 
+});
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
