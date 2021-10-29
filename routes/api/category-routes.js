828693e27959c9cb062660de6a255b38929d83a2
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-28 19:58:03 -0700

    error in categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,20 +2,20 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-  // find all categories
-  // be sure to include its associated Products
-  router.get('/', async (req, res) => {
-    try {
-      const result = await Category.findAll({ include: [{ model: Product }] });
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+// find all categories
+// be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const result = await Category.findAll({ include: [{ model: Product }] });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+ // find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+ 
   try {
     const result = await Category.findAll({
       where: {
@@ -25,29 +25,54 @@ router.get('/:id', (req, res) => {
     });
 
     if (!result) {
-      res.status(500).json({ message: "Non-exsistant product" });
+      res.status(500).json({ message: "Non-exsistant category" });
       return;
-    };
+    });
+  };
 
-router.post('/', (req, res) => {
-  // create a new category
-  router.post('/', async (req, res) => {
-    try {
-      const result = await Category.create({
-        category_name: req.body.category_name,
-      });
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(500).json({message: "Improper catefory input"});
-    }
-  });
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const result = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: "Improper category input" });
+  }
+});
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const result = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json('Invalid update');
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  router.delete('/:id', async (req, res) => {
+    try {
+      const result = await Category.destroy({
+        where: { id: req.params.id },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 });
 
 module.exports = router;
